861c423a5c2f4b2a2de6a68e7f85fd3c7a8ade46
Yaoo-Yaoo <151610360+Yaoo-Yaoo@users.noreply.github.com>
2024-02-26 19:23:07 +0800

    Save scene item data before transition and load it after transition.

--- Assets/Scripts/Inventory/Logic/ItemManager.cs
+++ Assets/Scripts/Inventory/Logic/ItemManager.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace MyFarm.Inventory
 {
@@ -8,27 +10,85 @@ namespace MyFarm.Inventory
 
         private Transform itemParent;
 
+        // 用于储存场景物品信息
+        private Dictionary<string, List<SceneItem>> sceneItemDict = new Dictionary<string, List<SceneItem>>();
+
         private void OnEnable()
         {
             EventHandler.InstantiateItemInScene += OnInstantiateItemInScene;
+            EventHandler.BeforeSceneUnloadEvent += OnBeforeSceneUnloadEvent;
             EventHandler.AfterSceneLoadedEvent += OnAfterSceneLoadedEvent;
         }
 
         private void OnDisable()
         {
             EventHandler.InstantiateItemInScene -= OnInstantiateItemInScene;
+            EventHandler.BeforeSceneUnloadEvent -= OnBeforeSceneUnloadEvent;
             EventHandler.AfterSceneLoadedEvent -= OnAfterSceneLoadedEvent;
-        }
-
-        private void OnAfterSceneLoadedEvent()
-        {
-            itemParent = GameObject.FindWithTag("ItemParent").transform;
         }
 
         private void OnInstantiateItemInScene(int ID, Vector3 pos)
         {
             Item item = Instantiate(itemPrefab, pos, Quaternion.identity, itemParent);
             item.itemID = ID;
+        }
+
+        private void OnBeforeSceneUnloadEvent()
+        {
+            GetAllSceneItems();
+        }
+
+        private void OnAfterSceneLoadedEvent()
+        {
+            itemParent = GameObject.FindWithTag("ItemParent").transform;
+            RecreateAllItems();
+        }
+
+        private void GetAllSceneItems()
+        {
+            List<SceneItem> currentSceneItems = new List<SceneItem>();
+
+            foreach (var item in FindObjectsOfType<Item>())
+            {
+                SceneItem sceneItem = new SceneItem
+                {
+                    itemID = item.itemID,
+                    position = new SerializableVector3(item.transform.position)
+                };
+
+                currentSceneItems.Add(sceneItem);
+            }
+
+            if (sceneItemDict.ContainsKey(SceneManager.GetActiveScene().name))
+            {
+                sceneItemDict[SceneManager.GetActiveScene().name] = currentSceneItems;
+            }
+            else
+            {
+                sceneItemDict.Add(SceneManager.GetActiveScene().name, currentSceneItems);
+            }
+        }
+
+        private void RecreateAllItems()
+        {
+            if (sceneItemDict.TryGetValue(SceneManager.GetActiveScene().name, out List<SceneItem> currentSceneItems))
+            {
+                if (currentSceneItems != null)
+                {
+                    // 清空场景中物品
+                    foreach (var item in FindObjectsOfType<Item>())
+                    {
+                        Destroy(item.gameObject);
+                    }
+
+                    // 生成新物品
+                    foreach (var item in currentSceneItems)
+                    {
+                        Item newItem = Instantiate(itemPrefab, item.position.ToVector3(), Quaternion.identity, itemParent);
+                        newItem.Init(item.itemID);
+                    }
+                }
+            }
         }
     }
 }
